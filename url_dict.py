6d2325d9f0fde51a6a0bbf784f11ca52a53f030d
Tanveer87 <18101025@uap-bd.edu>
2021-10-02 00:38:44 +0600

    Basic Design

---
+++ url_dict.py
@@ -0,0 +1,34 @@
+root_url = "https://the-ml-dl-app-bucket.s3.amazonaws.com/static/statistics_images/"
+
+url_dict = {
+    # House Price Prediction
+    '1' : {
+        "source_code_link" : "https://github.com/Tanveer87/HousePriceForecast",
+        "dataset_link" : "https://www.kaggle.com/c/house-prices-advanced-regression-techniques",
+        "stats_images_list" : {
+            "Bar Plots" : [
+                f"{root_url}1_bar_1.png", f"{root_url}1_bar_2.png",
+                f"{root_url}1_bar_3.png", f"{root_url}1_bar_4.png",
+                f"{root_url}1_bar_5.png", f"{root_url}1_bar_6.png",
+                f"{root_url}1_bar_7.png", f"{root_url}1_bar_8.png",
+                f"{root_url}1_bar_9.png", f"{root_url}1_bar_10.png"
+            ],
+            "Box Plots" : [
+                f"{root_url}1_box_1.png", f"{root_url}1_box_2.png",
+                f"{root_url}1_box_3.png", f"{root_url}1_box_4.png",
+                f"{root_url}1_box_5.png", f"{root_url}1_box_6.png",
+                f"{root_url}1_box_7.png", f"{root_url}1_box_8.png",
+                f"{root_url}1_box_9.png", f"{root_url}1_box_10.png"
+            ],
+            "Scatter Plots" : [
+                f"{root_url}1_scatter_1.png", f"{root_url}1_scatter_2.png",
+                f"{root_url}1_scatter_3.png"
+            ],
+            "Distibution Plots" : [
+                f"{root_url}1_dist_1.png", f"{root_url}1_dist_2.png",
+                f"{root_url}1_dist_3.png"
+            ]
+        }
+    },
+
+}
